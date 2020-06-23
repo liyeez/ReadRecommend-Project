749368388add4a3a01bac55b4d5e7aa43a91a179
chejn <41510168+chejn@users.noreply.github.com>
2020-06-24 01:09:27 +1000

    Added draft for book object

--- backend/project/api/models.py
+++ backend/project/api/models.py
@@ -19,3 +19,22 @@ class User(models.Model):
     def __str__(self):
         return self.email + ' ' + self.first_name + ' ' + self.last_name
 '''
+
+
+class BookManager(models.Manager):
+    def create_book(self, title, author, isbn, pub_date):
+        book = self.create(title=title, author=author,
+                           isbn=isbn, pub_date=pub_date)
+        # do something with the book
+        return book
+    # def get_reviews(isbn):
+    #
+
+
+class Book(models.Model):
+    cover = models.CharField(max_length=128)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13)
+    pub_date = models.DateField()
+    objects = BookManager()
