297a03af25d864c3ec6dccd3086940b57873a733
chejn <41510168+chejn@users.noreply.github.com>
2020-06-25 13:27:14 +1000

    update books

--- backend/project/api/books.py
+++ backend/project/api/books.py
@@ -3,24 +3,27 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from api.models import Book, BookManager
+from django.core import serializers
 
 
 @api_view(["GET"])
 def data(request):
 
     # Input validation
-    print(request.GET.get('isbn'), False)
+   # print(request.GET.get('isbn'), False)
     try:
         isbn = request.GET["isbn"]
        # print(isbn)
     except:
         return Response({"status": "error", "message": "Invalid request"}, status=status.HTTP_400_BAD_REQUEST)
-    if isbn:
+    if isbn == '':
         return Response({"status": "error", "message": "Fields cannot be blank"}, status=status.HTTP_400_BAD_REQUEST)
 
     try:
-        bookData = Book.objects.get(pk=isbn)
+        Book.objects.get(pk=isbn)
     except:
         return Response({"status": "error", "message": "Isbn not found"}, status=status.HTTP_200_OK)
-
-    return Response({"status": "ok", "message": "Isbn found success", "bookData": bookData}, status=status.HTTP_200_OK)
+    # print(bookData.title)
+    book_data_json = serializers.serialize(
+        'json', Book.objects.filter(pk=isbn))
+    return Response({"status": "ok", "message": "Isbn found success", "data": book_data_json}, status=status.HTTP_200_OK)
