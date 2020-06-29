552a93ed4ac8b43571a57258d46f14baef3a5c41
Jiayi <jiayi.li1@outlook.com>
2020-06-29 18:25:40 +1000

    api/collections.py

--- backend/project/api/collections.py
+++ backend/project/api/collections.py
@@ -3,15 +3,13 @@ from rest_framework.response import Response
 from rest_framework import status
 from api.models import Collection, Book
 from django.core import serializers
+from .utilities import input_validator
 
 @api_view(["GET"])
+@input_validator(["collection_id"])
 def view_collection(request): #given collection id returns collection data
-    try:
+    try: #check collection exists
         c_id = request.GET["collection_id"]
-    except:
-        return Response({"status": "error", "message": "Invalid request"}, status=status.HTTP_400_BAD_REQUEST)
-
-    try:
         Collection.objects.get(pk=c_id)
     except:
         return Response({"status": "error", "message": "Collection not found"}, status=status.HTTP_200_OK)
@@ -21,22 +19,19 @@ def view_collection(request): #given collection id returns collection data
 
 
 @api_view(["GET"])
+@input_validator(["collection_id", "isbn"])
 def add_title(request): #given collection_id and isbn, adds book to collection
-    try: 
+    try: #check collection exists
         c_id = request.GET["collection_id"]
-        isbn = request.GET["isbn"]
-    except:
-        return Response({"status": "error", "message": "Invalid request"}, status=status.HTTP_400_BAD_REQUEST)
-
-    try:
         c = Collection.objects.get(pk=c_id)
     except:
         return Response({"status": "error", "message": "Collection not found"}, status=status.HTTP_200_OK)
-    try:
+    try: #check book exists
+        isbn = request.GET["isbn"]
         book = Book.objects.get(pk = isbn)
     except:
         return Response({"status": "error", "message": "Book not found"}, status=status.HTTP_200_OK)
-    try: #error if book already in collection
+    try: #check if book already in collection
         c.books.get(pk = isbn)
         return Response({"status": "error", "message": "Book is already in collection"}, status=status.HTTP_200_OK)
     except:
@@ -47,19 +42,15 @@ def add_title(request): #given collection_id and isbn, adds book to collection
         return Response({"status": "ok", "message": "Book added to collection", "data": collection_json}, status=status.HTTP_200_OK)
 
 @api_view(["GET"])
+@input_validator(["collection_id", "isbn"])
 def delete_title(request): #given collection_id and isbn, removes book from collection
-    try: 
-        c_id = request.GET["collection_id"]
-        isbn = request.GET["isbn"]
-        print("isbn "+ isbn)
-    except:
-        return Response({"status": "error", "message": "Invalid request"}, status=status.HTTP_400_BAD_REQUEST)
-
     try:
+        c_id = request.GET["collection_id"]
         c = Collection.objects.get(pk=c_id)
     except:
         return Response({"status": "error", "message": "Collection not found"}, status=status.HTTP_200_OK)
     try:
+        isbn = request.GET["isbn"]
         book = Book.objects.get(pk = isbn)
     except:
         return Response({"status": "error", "message": "Book not found"}, status=status.HTTP_200_OK)
