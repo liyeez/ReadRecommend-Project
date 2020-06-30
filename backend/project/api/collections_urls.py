7c5d0fc0a3b66a6a5714975f82e86e134737f665
Jiayi <jiayi.li1@outlook.com>
2020-06-30 16:23:12 +1000

    fixed bug in add_title, entry point for add to library

--- backend/project/api/collections_urls.py
+++ backend/project/api/collections_urls.py
@@ -4,6 +4,7 @@ from . import collections
 urlpatterns = [
 	path("create_collection", collections.create_collection),
     path("view_collection", collections.view_collection),
+    path("add_to_library", collections.add_title),
     path("add_title", collections.add_title),
     path("delete_from_library", collections.delete_from_library),
     path("delete_title", collections.delete_title),
